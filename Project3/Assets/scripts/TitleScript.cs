f16ba03600b59081833329ed71fe081aba724b95
txm1918 <txm1918@IGM2435-06>
2018-11-16 10:49:16 -0500

    Started Title

---
+++ Project3/Assets/scripts/TitleScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScript : MonoBehaviour {
+
+    Vector3 position;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GoToChapterSelect()
+    {
+        
+    }
+
+    void GoToOptions()
+    {
+
+    }
+
+    void GoToCredits()
+    {
+
+    }
+
+    void Quit()
+    {
+
+    }
+}
