3f7cad4caa2cf66b929e9802ca2eca86eb84598a
Gilexarr <tmenichel@gmail.com>
2018-11-26 10:52:30 -0500

    added some spells that are not implemented

--- Project3/Assets/scripts/Combat/SpellManager.cs
+++ Project3/Assets/scripts/Combat/SpellManager.cs
@@ -12,19 +12,19 @@ public class SpellManager : MonoBehaviour {
         abilitiesDictionary.Add("StaffAttack", new string[1] { "damage" });
     }
 
-    public string CallAbility(string abilityName, GameObject origin, GameObject otherCreature, List<float> values)
+    public string CallAbility(string abilityName, GameObject origin, GameObject target, List<float> values)
     {
         string whatHappened;
         switch (abilityName)
         {
             case "PistolShot":
-                whatHappened = PistolShot(origin, otherCreature, values[0]);
+                whatHappened = PistolShot(origin, target, values[0]);
                 break;
             case "StaffAttack":
-                whatHappened = StaffAttack(origin, otherCreature, values[0]);
+                whatHappened = StaffAttack(origin, target, values[0]);
                 break;
             case "BasicAttack":
-                whatHappened = StaffAttack(origin, otherCreature, values[0]);
+                whatHappened = StaffAttack(origin, target, values[0]);
                 break;
             default:
                 whatHappened = "Nothing";
@@ -35,41 +35,167 @@ public class SpellManager : MonoBehaviour {
     /// <summary>
     /// deals basic amounts of damage with fisty boys
     /// </summary>
-    /// <param name="otherCreature"></param>
+    /// <param name="target"></param>
     /// <param name="attackMod"></param>
-    public string BasicAttack(GameObject origin, GameObject otherCreature, float attackMod)
+    public string BasicAttack(GameObject origin, GameObject target, float attackMod)
     {
-        Creature otherStats = otherCreature.GetComponent<Creature>();
-        int damage = (int)(6 * attackMod);
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(10);
         otherStats.currentHealth -= damage;
-        return origin.name + " did " + damage + " damage to " + otherCreature.name;
+        return origin.name + " did " + damage + " damage to " + target.name;
     }
     /// <summary>
     /// reduces ammo of the person casting this
     /// deals big damage
     /// </summary>
     /// <param name="origin"></param>
-    /// <param name="otherCreature"></param>
+    /// <param name="target"></param>
     /// <param name="attackMod"></param>
-    public string PistolShot(GameObject origin, GameObject otherCreature, float attackMod)
+    public string PistolShot(GameObject origin, GameObject target, float attackMod)
     {
-        Creature otherStats = otherCreature.GetComponent<Creature>();
+        Creature otherStats = target.GetComponent<Creature>();
         Debug.Log(attackMod);
-        int damage = (int)(10 * attackMod);
+        int damage = (int)(15);
         otherStats.currentHealth -= damage;
         origin.GetComponent<Creature>().Ammo--;
-        return origin.name + " used 1 ammo to deal " + damage + " damage to " + otherCreature.name;
+        return origin.name + " used 1 ammo to deal " + damage + " damage to " + target.name;
     }
     /// <summary>
     /// deals medium damage with a staffy boys
     /// </summary>
-    /// <param name="otherCreature"></param>
+    /// <param name="target"></param>
     /// <param name="attackMod"></param>
-    public string StaffAttack(GameObject origin, GameObject otherCreature, float attackMod)
+    public string StaffAttack(GameObject origin, GameObject target, float attackMod)
     {
-        Creature otherStats = otherCreature.GetComponent<Creature>();
-        int damage = (int)(8 * attackMod);
-        otherStats.currentHealth -= (int)(8 * attackMod);
-        return origin.name + " did " + damage + " damage to " + otherCreature.name;
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(10);
+        otherStats.currentHealth -= damage;
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Fuego(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+
+    /// <summary>
+    /// stuff below needs changing
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public string CircleOfFire(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        Creature originStats = origin.GetComponent<Creature>();
+        originStats.Attack *= 1.3f;
+        return origin.name + " increased it's attack by 30%!";
+    }
+    public string COfIron(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string EnforceGuard(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Fortius(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Defendarius(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Slash(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Fortify(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Swipe(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Siphon(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Screech(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Bite(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Scorch(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Howl(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Scratch(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string VamprieBite(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
+    }
+    public string Feast(GameObject origin, GameObject target)
+    {
+        Creature otherStats = target.GetComponent<Creature>();
+        int damage = (int)(20);
+        otherStats.currentHealth -= (int)(damage);
+        return origin.name + " did " + damage + " damage to " + target.name;
     }
 }
