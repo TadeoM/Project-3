e9d690a75a7a6623a95f9c40ee353e0edb507b96
Sam Belisle <spb6892@rit.edu>
2018-12-16 18:52:56 -0500

    Combat UI and stuff

---
+++ Project3/Assets/scripts/UI/CombatUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatUI : MonoBehaviour {
+
+    public List<GameObject> abilities = new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Select()
+    {
+        for (int i = 0; i < abilities.Count; i++)
+        {
+            if (abilities[i].activeSelf)
+                abilities[i].SetActive(false);
+            else
+                abilities[i].SetActive(true);
+
+        }
+    }
+}
