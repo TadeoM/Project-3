1b4b7b9b210609c84d49fc3d15a9d5e55d2036f7
Gilexarr <tmenichel@gmail.com>
2018-12-16 20:09:16 -0500

    Audio!

---
+++ Project3/Assets/MusicManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagement : MonoBehaviour {
+
+    public AudioSource overworld;
+    public AudioSource combat;
+    public AudioClip[] combatClips;
+    CombatSwitch combatSwitcheroo;
+	// Use this for initialization
+	void Start () {
+        combatSwitcheroo = GetComponent<CombatSwitch>();
+        overworld.volume = 1.0f;
+        combat.volume = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(combatSwitcheroo.inCombat && combat.volume < 0.95f)
+        {
+            overworld.volume -= 0.05f;
+            combat.volume += 0.05f;
+        }
+        else if (!combatSwitcheroo.inCombat && overworld.volume < 0.95f)
+        {
+            overworld.volume += 0.05f;
+            combat.volume -= 0.05f;
+        }
+    }
+}
